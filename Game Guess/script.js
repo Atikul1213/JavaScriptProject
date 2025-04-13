fafc757697b24f7e9ae0f62292f1522a6e21e49c
Atikul1213 <atikul.islam@brainstation-23.com>
2025-04-13 15:52:20 +0600

    Add playlist project

--- Game Guess/script.js
+++ Game Guess/script.js
@@ -1,52 +1,29 @@
-/**
- 
 
-document.querySelector(".message").textContent = 'Correct Number !';
-
-document.querySelector(".number").textContent = 13;
-
-document.querySelector(".score").textContent = 10;
-
-
-document.querySelector(".guess").value = 23;
-
-*/
- 
 let target = Math.ceil(Math.random()*100);
 console.log("target: ",target);
-
-//document.querySelector(".number").innerHTML = target;
-
-
+ 
 var highscore = Number(document.querySelector(".highscore").innerHTML);
 console.log("highScore: ",highscore);
 console.log("type of highscore: ",typeof highscore);
-
 
 var score = Number(document.querySelector(".score").innerHTML);
 console.log("score: ",score);
 console.log("type of score: ",typeof score);
 
-
 document.querySelector(".check").addEventListener('click',function(){
-
     const guess = Number(document.querySelector(".guess").value);
     console.log("guess: ",guess);
     console.log("type of guess: ",typeof guess);
-
 
     if(score<1){
         document.querySelector(".message").innerHTML = "You lost the game!";
         return;
     }
 
-
     if(!guess){
         document.querySelector(".message").innerHTML = "No number!";
     }
-
     else{
-
         if(guess===target){
             document.querySelector(".message").innerHTML = "Correct Number!";
             document.querySelector("body").style.backgroundColor="#60b347";
@@ -56,14 +33,11 @@ document.querySelector(".check").addEventListener('click',function(){
                 highscore = score;
                 document.querySelector(".highscore").innerHTML = highscore;
             }
-            
-
         }
         else if(target<guess){
             document.querySelector(".message").innerHTML = "Too high!";
             score--;
             document.querySelector(".score").innerHTML = score;
-            
         }
         else{
             document.querySelector(".message").innerHTML = "Too low!";
@@ -71,19 +45,8 @@ document.querySelector(".check").addEventListener('click',function(){
             document.querySelector(".score").innerHTML = score;           
         }
     }
-
-
     document.querySelector(".guess").value="";
-
-
-
-    
 });
-
-
-
-
-
 
 document.querySelector(".again").addEventListener('click',function(){
     score = 20;
@@ -95,6 +58,78 @@ document.querySelector(".again").addEventListener('click',function(){
 
     document.querySelector("body").style.backgroundColor = "#222";
     document.querySelector(".number").style.width = '15rem';
-
-
 });
+
+/*
+
+let checkBtn = document.querySelector('.check');
+let guess = document.querySelector('.guess');
+let  msg = document.querySelector('.message');
+let num = document.querySelector(".number");
+let secretNumber = Math.ceil(Math.random()*100);
+let score = document.querySelector(".score");
+let scoreValue = 20;
+let hscore = 0;
+let highscore = document.querySelector('.highscore');
+console.log("secretNumber: ",secretNumber);
+console.log("score: ",score.textContent);
+console.log("checkbtn: ",checkBtn);
+console.log("guess: ",guess);
+//num.textContent = secretNumber;
+checkBtn.addEventListener("click",function(){
+    console.log("click the button", guess.value);
+    console.log("type: ", typeof guess.value);
+
+    if(!guess.value){
+        msg.textContent = 'No number';
+        console.log("msg content: ",msg.textContent);
+    }
+    else{
+        if(guess.value > secretNumber){
+            console.log("Greater");
+            msg.textContent = 'Greater....';
+            scoreValue = scoreValue - 1;
+            score.textContent = scoreValue;
+        }
+        else if(guess.value < secretNumber){
+            console.log("Smaller");
+            msg.textContent = 'Smaller....';
+            scoreValue = scoreValue - 1;
+            score.textContent = scoreValue;
+        }
+        else{
+            console.log("Correct");
+            msg.textContent = 'Correct...';
+            document.querySelector('body').style.backgroundColor= "#60b347";
+            num.style.width = '30rem';
+            num.textContent = secretNumber;
+            if(scoreValue < hscore)
+            {
+                hscore = scoreValue;
+                highscore.textContent = hscore;
+            }
+
+        }
+    }
+});
+
+
+
+let again = document.querySelector('.again');
+again.addEventListener('click',function(){
+    console.log("call the again button");
+    scoreValue = 20;
+    secretNumber = Math.ceil(Math.random()*100);
+    msg.textContent = 'Start guessing.....';
+    score.textContent = scoreValue;
+    num.textContent = '?';
+    guess.value = '';
+});
+
+
+
+
+
+
+*/
+
--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,32 +1,32 @@
 const modal = document.querySelector(".modal");
-
 const overlay = document.querySelector(".overlay");
-
 const btnCloseModal = document.querySelector('.close-modal');
-
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-console.log("btnopenModal: ",btnsOpenModal);
+//console.log("btnopenModal: ",btnsOpenModal);
+
 btnsOpenModal.forEach(x=>{
 
-    console.log("click x: ",x);
+    //console.log("click x: ",x);
     x.addEventListener('click',function(){
        // modal.style.display = 'block';
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
-        
     });
 });
 
-
-
 const closeModel = function(){
-    
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
 
-
-
 btnCloseModal.addEventListener('click',closeModel);
 overlay.addEventListener('click',closeModel);
+
+document.addEventListener('keydown', function(e){
+    console.log('any key pressed', e.key);
+    if(e.key === "Escape" && !modal.classList.contains('hidden')){
+        console.log("Escape key pressed");
+        closeModel();
+    }
+});
